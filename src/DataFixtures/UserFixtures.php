6606f308cd367bb7da403d7574cfefd915d19aa4
Drx85 <ced.dep.85@hotmail.fr>
2021-08-02 20:55:19 +0200

    Feat: add authentification system

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+	private $passwordHasher;
+
+     public function __construct(UserPasswordHasherInterface $passwordHasher)
+     {
+         $this->passwordHasher = $passwordHasher;
+     }
+
+    public function load(ObjectManager $manager)
+    {
+		$user = new User();
+		$user->setUsername('admin');
+		$user->setEmail('test@test.fr');
+		$user->setRoles((array)'ROLE_ADMIN');
+		$user->setPassword($this->passwordHasher->hashPassword($user, 'demo'));
+		$manager->persist($user);
+        $manager->flush();
+    }
+}
