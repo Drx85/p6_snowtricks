48b17d16b0130693e64c5dabc4ec63a2fd47edc3
Drx85 <ced.dep.85@hotmail.fr>
2021-08-19 19:27:03 +0200

    Customize Error Pages

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -24,7 +24,7 @@ class Category
     /**
      * @ORM\Column(type="string", length=255)
      */
-    private $name;
+	private ?string $name;
 
     /**
      * @ORM\OneToMany(targetEntity=Trick::class, mappedBy="category")
